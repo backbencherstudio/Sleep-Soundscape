af83a2d2675eb3863fcadde012e4e9fe4cc74466
wahabreja-cmd <awahabreja1@gmail.com>
2025-02-20 12:02:02 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -71,3 +71,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// echo "# sleep-soundscape" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/backbencherstudio/sleep-soundscape.git
+// git push -u origin main
+//
+//
+//
+// git remote add origin https://github.com/backbencherstudio/sleep-soundscape.git
+// git branch -M main
+// git push -u origin main
