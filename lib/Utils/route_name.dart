2d49eb61253f283f1a5d388c93445127bf26a58d
Habibul Bashar <habibulbashar@Habibuls-Mac-mini.local>
2025-02-24 09:58:01 +0600

    routes added

--- lib/Utils/route_name.dart
+++ lib/Utils/route_name.dart
@@ -1,7 +1,9 @@
 class RouteName {
   static const splashScreen = "/splashScreen";
   static const onboardingScreen = "/onboardingScreen";
-  static const loginScreen = "/loginScreen";
+  static const signUpScreen = "/signUpScreen";
+  static const signInScreen = "/signInScreen";
+  static const forgotPassword = "/forgotPassword";
   static const parentScreens = "/parentScreens";
   static const homeScreen = "/homeScreen";
   static const profileScreen = "/profileScreen";
--- lib/view/onboarding_screen/onboarding_screen.dart
+++ lib/view/onboarding_screen/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sleep_soundscape/Utils/route_name.dart';
 import 'package:sleep_soundscape/view/parent_screen/screen/parent_screen.dart';
 import '../../global_widget/custom_button.dart';
 import '../home_screen/screen/home_screen.dart'; // Import Home Screen
@@ -53,12 +54,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         curve: Curves.easeInOut,
       );
     } else {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const ParentScreens(),
-        ),
-      );
+      Navigator.pushNamedAndRemoveUntil(context, RouteName.signUpScreen, (_)=>false);
     }
   }
 
