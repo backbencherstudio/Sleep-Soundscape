5c429ded09e5e1eb789119b9d7fc02335f450e61
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-02-25 12:44:20 +0600

    theme mode added in login screen

--- lib/view/Login_Screen/widget/inputDecoration.dart
+++ lib/view/Login_Screen/widget/inputDecoration.dart
@@ -3,32 +3,38 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 InputDecoration inputDecoration({required  BuildContext context,  Widget? suffixIcon, String? labelText, String? hintText, Widget? prefixIcon,}) {
   return InputDecoration(
-    fillColor: Colors.white.withOpacity(0.04),
+    fillColor: Theme.of(context).inputDecorationTheme.fillColor,
+    //Colors.white.withOpacity(0.04),
     filled: true,
     labelText: labelText,
-    labelStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-      color: Color(0xFFFFFFFFF).withOpacity(0.6),
-    ),
+    labelStyle: Theme.of(context).inputDecorationTheme.labelStyle,
+    // textTheme.bodyMedium!.copyWith(
+    //   color: Color(0xFFFFFFFFF).withOpacity(0.6),
+    // ),
     hintText: hintText,
-    hintStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-      color: Color(0xFFFFFFFFF).withOpacity(0.6),
-    ),
-    floatingLabelStyle: Theme.of(
-      context,
-    ).textTheme.bodySmall!.copyWith(color: Color(0xFF4B5155), fontSize: 14.sp),
-    enabledBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(14.r),
-      borderSide: BorderSide(color: Color(0xFF4B5155).withOpacity(0.6)),
-    ),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(14.r),
-      borderSide: BorderSide(color: Color(0xFFFAD051)),
-    ),
-    errorBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(14.r),
-      borderSide: BorderSide(color: Color(0xFF9BA2A7)),
-    ),
+    hintStyle: Theme.of(context).inputDecorationTheme.hintStyle,
+    // textTheme.bodyMedium!.copyWith(
+    //   color: Color(0xFFFFFFFFF).withOpacity(0.6),
+    // ),
+    floatingLabelStyle: Theme.of(context).inputDecorationTheme.labelStyle,
+    enabledBorder: Theme.of(context).inputDecorationTheme.enabledBorder,
+    // OutlineInputBorder(
+    //   borderRadius: BorderRadius.circular(14.r),
+    //   borderSide: BorderSide(color: Color(0xFF4B5155).withOpacity(0.6)),
+    // ),
+    focusedBorder:Theme.of(context).inputDecorationTheme.focusedBorder,
+    // OutlineInputBorder(
+    //   borderRadius: BorderRadius.circular(14.r),
+    //   borderSide: BorderSide(color: Color(0xFFFAD051)),
+    // ),
+    errorBorder: Theme.of(context).inputDecorationTheme.errorBorder,
+    // OutlineInputBorder(
+    //   borderRadius: BorderRadius.circular(14.r),
+    //   borderSide: BorderSide(color: Color(0xFF9BA2A7)),
+    // ),
     prefixIcon:prefixIcon ,
     suffixIcon: suffixIcon,
+    prefixIconColor: Theme.of(context).inputDecorationTheme.prefixIconColor,
+    suffixIconColor: Theme.of(context).inputDecorationTheme.suffixIconColor,
   );
 }
