1e517864874118c12a17feae874e8f2f4ec5170a
wahabreja-cmd <awahabreja1@gmail.com>
2025-02-26 12:18:32 +0600

    update code

--- lib/api_services/api_end_point.dart
+++ lib/api_services/api_end_point.dart
@@ -3,7 +3,7 @@ class AppUrls {
   //https://energy-backend.ddns.net
   static const String baseUrl = 'http://192.168.40.25:3000/api';
   //'https://dependent-boxes-eh-mandate.trycloudflare.com';
-  static  String sound(category) => '$baseUrl/sounds/filterSounds?category=fire&${category}=abcd2';
+  static String sound(String category) => '$baseUrl/sounds/filterSounds?category=$category';
   //static String readNotification(id) => '$baseUrl/notification/mark-read/${id}';
 
 
