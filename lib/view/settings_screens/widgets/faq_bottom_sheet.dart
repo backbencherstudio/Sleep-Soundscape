a6f9aa6f9ca7c361dd82db11f6574bf2a639ea7a
salmanbashar1009 <salmanbashar101@gmail.com>
2025-02-26 17:13:35 +0600

    updated

--- lib/view/settings_screens/widgets/faq_bottom_sheet.dart
+++ lib/view/settings_screens/widgets/faq_bottom_sheet.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:sleep_soundscape/global_widget/custom_button.dart';
+import 'package:sleep_soundscape/model_view/theme_provider.dart';
 void faqBottomSheet(BuildContext context) {
   showModalBottomSheet(
     isScrollControlled: true,
     context: context,
-    backgroundColor: const Color(0xff0F0F14),
+    // backgroundColor: const Color(0xff0F0F14),
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
     ),
     builder: (BuildContext context) {
+
+      final darkTheme = context.watch<ThemeProvider>().isDarkMode;
+
       return Container(
         height: MediaQuery.of(context).size.height * 0.9, // 90% of screen height
         decoration: BoxDecoration(
-          color: const Color(0xff0F0F13),
+          // color: const Color(0xff0F0F13),
           borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
         ),
         padding: EdgeInsets.all(16.w),
@@ -29,9 +35,9 @@ void faqBottomSheet(BuildContext context) {
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 7.w),
                     decoration: BoxDecoration(
-                      color: Colors.transparent,
+                      // color: Colors.transparent,
                       border: Border.all(
-                        color: const Color.fromRGBO(255, 255, 255, 0.20),
+                        color: darkTheme ? Color.fromRGBO(255, 255, 255, 0.20) : Color.fromRGBO(0, 0, 0, 0.20),
                         width: 1,
                       ),
                       shape: BoxShape.circle,
@@ -42,8 +48,8 @@ void faqBottomSheet(BuildContext context) {
                         offset: const Offset(2, 0),
                         child: Icon(
                           Icons.arrow_back_ios,
-                          color: Colors.white,
-                          size: 10.sp,
+                          color: darkTheme ? Colors.white : Colors.black,
+                          size: 10.r,
                         ),
                       ),
                     ),
@@ -51,15 +57,12 @@ void faqBottomSheet(BuildContext context) {
                 ),
                 SizedBox(width: 100.w),
                 Expanded(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "FAQs",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w600,
-                      ),
+                  child: Text(
+                    "FAQs",
+                    style: TextStyle(
+
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
@@ -79,29 +82,13 @@ void faqBottomSheet(BuildContext context) {
                 ),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: const Color.fromRGBO(255, 255, 255, 0.08),
-                        width: 1.0,
-                      ),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: const Color.fromRGBO(255, 255, 255, 0.08),
-                        width: 1.0,
-                      ),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    filled: true,
-                    fillColor: const Color(0xff19191C),
                     prefixIcon: Icon(
                       Icons.search,
-                      color: const Color.fromRGBO(255, 255, 255, 0.50),
+                      color: darkTheme ? const Color.fromRGBO(255, 255, 255, 0.50) : const Color.fromRGBO(0, 0, 0, 0.50)
                     ),
                     hintText: "search",
                     hintStyle: TextStyle(
-                      color: const Color.fromRGBO(255, 255, 255, 0.50),
+                      color: darkTheme ? const Color.fromRGBO(255, 255, 255, 0.50) : const Color.fromRGBO(0, 0, 0, 0.50),
                       fontSize: 14.sp,
                     ),
                   ),
@@ -112,18 +99,21 @@ void faqBottomSheet(BuildContext context) {
             TextIconRow(text: "Lorem ipsum dolor sit amet"),
             SizedBox(height: 24.h),
 
-            Divider(color:Color.fromRGBO(255, 255, 255, 0.10),height: 1,),
+            Divider(color:darkTheme ? Color.fromRGBO(255, 255, 255, 0.10) : Color.fromRGBO(0, 0, 0, 0.10),height: 1,),
             SizedBox(height: 24.h),
             TextIconRow(text: "Lorem ipsum dolor sit amet"),
             SizedBox(height: 24.h),
 
-            Divider(color:Color.fromRGBO(255, 255, 255, 0.10),height: 1,),
+            Divider(color:darkTheme ? Color.fromRGBO(255, 255, 255, 0.10) : Color.fromRGBO(0, 0, 0, 0.10),height: 1,),
 
             SizedBox(height: 24.h),
             TextIconRow(text: "Lorem ipsum dolor sit amet"),
             SizedBox(height: 24.h),
 
-            Divider(color:Color.fromRGBO(255, 255, 255, 0.10),height: 1,),
+            Divider(color:darkTheme ? Color.fromRGBO(255, 255, 255, 0.10) : Color.fromRGBO(0, 0, 0, 0.10),height: 1,),
+            SizedBox(height: 32.h,),
+
+            CustomButton(text: "Continue", onPressed: (){})
 
           ],
         ),
@@ -150,11 +140,11 @@ class TextIconRow extends StatelessWidget {
         children: [
           Text(
             text,
-            style: TextStyle(color: Colors.white, fontSize: 14.sp),
+            style: Theme.of(context).textTheme.bodyMedium,
           ),
           Icon(
             icon,
-            color: Colors.white,
+
             size: 24.sp,
           ),
         ],
