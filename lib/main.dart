7d3303585fe36b9e7686473d24c4dd6a505644d8
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-02-28 09:40:50 +0600

    alarm fixed

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   await AndroidAlarmManager.initialize();
+  initializeTimeZones();
   await initializeService();
 
-  initializeTimeZones();
+
 
   final prefs = await SharedPreferences.getInstance();
   String? token = prefs.getString("token");
