d2025dbcb4f17d2e1bbe1f63e05fb5834bb299f1
salmanbashar1009 <salmanbashar101@gmail.com>
2025-03-01 08:28:46 +0600

    updated

--- lib/api_services/api_end_point.dart
+++ lib/api_services/api_end_point.dart
@@ -3,12 +3,13 @@ class AppUrls {
 
   //https://energy-backend.ddns.net
   static const String baseUrl = 'http://192.168.40.10:1000';
+
   //'https://dependent-boxes-eh-mandate.trycloudflare.com';
-  static String sound(String category) => '$baseUrl/sounds/filterSounds?category=$category';
+  static String sound(String category) =>
+      '$baseUrl/sounds/filterSounds?category=$category';
+
   //static String readNotification(id) => '$baseUrl/notification/mark-read/${id}';
-   static String forgotPassword  = '$baseUrl/api/users/forgot-password';
+  static String forgotPassword = '$baseUrl/api/users/forgot-password';
   static String loginUrl = '$baseUrl/api/users/logIn';
   static String signUp = '$baseUrl/api/users/signup';
-
-
 }
